c6938b2cc3a148728d31bb846a862458243a11f3
hugovrp <hugovrp@gmail.com>
2026-09-08 17:44:09 -0300

    removing overlay bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -62,7 +62,6 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       className={`${roboto.variable} ${ibmPlexSerif.variable} h-full antialiased`}
     >
       <body>
-        <AosSetup />
         <main>
           {children}
         </main>
