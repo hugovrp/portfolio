be85ba7bbf1de718b4c1d384441aefa4886b43c9
hugovrp <hugovrp@gmail.com>
2026-09-12 17:11:35 -0300

    adjusting

--- data/experience.ts
+++ data/experience.ts
@@ -1,7 +1,7 @@
 export const experience = [
   {
     role: 'Estagiário de TI', 
-    description: 'Responsável pelo desenvolvimento do site institucional, suporte em TI e elaboração de materiais educativos para promover a inclusão digital de idosos.',
+    description: 'Responsável pelo desenvolvimento do site institucional, suporte em TI e elaboração de materiais educativos para promover a inclusão digital de pessoas idosas.',
     enterprise: 'IVERT',
     period: '03/2026 - Atual'
   },
